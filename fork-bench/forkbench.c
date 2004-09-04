8eb89fb486fca4f34794b6f4b7a4bfd6eee7b899
hjp <hjp>
2004-09-04 09:43:20 +0000

    *** empty log message ***

---
+++ fork-bench/forkbench.c
@@ -0,0 +1,118 @@
+#include <assert.h>
+#include <errno.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/wait.h>
+
+char *cmnd;
+
+void usage(void) {
+        fprintf(stderr, "Usage: %s -p maxproc -w maxwork\n", cmnd);
+        exit(1);
+}
+
+int main(int argc, char **argv) {
+        int i;
+        int max_proc = 10;
+        int max_work = 0;
+        struct {
+                int type;
+                int pid;
+                int outstanding;
+        } *event;
+        int ec = 0;
+        int pid;
+        int outstanding = 0;
+        int c;
+        int max_outstanding = 0;
+        int sum_outstanding = 0;
+        int failed_forks = 0;
+
+        while ((c = getopt(argc, argv, "p:w:")) != EOF) {
+                switch (c) {
+                        char *p;
+                case 'p':
+                        max_proc = strtol(optarg, &p, 0);
+                        if (p == optarg || *p) usage();
+                        break;
+                case 'w':
+                        max_work = strtol(optarg, &p, 0);
+                        if (p == optarg || *p) usage();
+                        break;
+                case '?':
+                        usage();
+                default:
+                        assert(0);
+                }
+
+        }
+        event = malloc(max_proc * 2 * sizeof(*event));
+        if (!event) {
+                fprintf(stderr,
+                        "%s: cannot malloc event log: %s\n",
+                        argv[0], strerror(errno));
+                exit(1);
+        }
+
+        for (i = 0; i < max_proc; i++) {
+                switch (pid = fork()) {
+                case -1:
+                        assert(ec < 2*max_proc);
+                        event[ec].type = 0;
+                        event[ec].pid = pid;
+                        event[ec].outstanding = outstanding;
+                        ec++;
+                        break;
+                case 0:
+                        for (i = 0; i < max_work; i++) ;
+                        exit(0);
+                        break;
+                default:
+                        outstanding++;
+                        assert(ec < 2*max_proc);
+                        event[ec].type = 0;
+                        event[ec].pid = pid;
+                        event[ec].outstanding = outstanding;
+                        ec++;
+                        while ((pid = wait3(NULL, WNOHANG, NULL)) > 0) {
+                                outstanding--;
+                                assert(ec < 2*max_proc);
+                                event[ec].type = 1;
+                                event[ec].pid = pid;
+                                event[ec].outstanding = outstanding;
+                                ec++;
+                        }
+                }
+        }
+        while ((pid = wait3(NULL, 0, NULL)) > 0) {
+                outstanding--;
+                assert(ec < 2*max_proc);
+                event[ec].type = 1;
+                event[ec].pid = pid;
+                event[ec].outstanding = outstanding;
+                ec++;
+        }
+        for (i = 0; i < ec; i++) {
+                printf("%d: %s %d (%d outstanding)\n",
+                        i,
+                        event[i].type ? "exit" : "fork",
+                        event[i].pid,
+                        event[i].outstanding);
+                if (event[i].outstanding > max_outstanding) {
+                        max_outstanding = event[i].outstanding;
+                }
+                sum_outstanding += event[i].outstanding;
+                if (event[i].type == 0 && event[i].pid == -1) {
+                        failed_forks ++;
+                }
+        }
+        printf ("\n");
+        printf ("max outstanding: %d, avg outstanding: %g\n",
+                max_outstanding, (double)sum_outstanding / ec);
+        printf ("failed forks: %d\n", failed_forks);
+        exit(0);
+}
+
+
+
---
+++ fork-bench/forkbench2.c
@@ -0,0 +1,49 @@
+#include <assert.h>
+#include <errno.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/wait.h>
+#include <sys/time.h>
+
+char *cmnd;
+
+void usage(void) {
+        fprintf(stderr, "Usage: %s -p maxproc -w maxwork\n", cmnd);
+        exit(1);
+}
+
+int main(int argc, char **argv) {
+        int i;
+	int n;
+	int f = 0;
+	int s = 0;
+	struct timeval tv0, tv1;
+
+	if (argc == 2) {
+		n = strtoul(argv[1], NULL, 0);
+	} else {
+		usage();
+	}
+	gettimeofday(&tv0, NULL);
+	for (i = 0; i < n; i++) {
+		pid_t pid;
+                switch (pid = fork()) {
+		case -1:
+			f++;
+			break;
+		case 0:
+			exit(0);
+		default:
+			while (wait3(NULL, WNOHANG, NULL) > 0);
+			s++;
+		}
+	}
+	gettimeofday(&tv1, NULL);
+	printf("%d successful forks, %d failures: %g seconds\n",
+			s, f,
+			(tv1.tv_sec + tv1.tv_usec * 1E-6) -
+			(tv0.tv_sec + tv0.tv_usec * 1E-6)
+	      );
+	exit(0);
+}
