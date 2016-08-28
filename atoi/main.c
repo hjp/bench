8c1f7416edfb2105efcc87c6158a7fde6181b425
Peter J. Holzer <hjp@hjp.at>
2016-08-28 23:00:11 +0200

    Run loop 1000 times

--- atoi/main.c
+++ atoi/main.c
@@ -17,15 +17,17 @@ int main(void) {
 
     struct timeval t0;
     gettimeofday(&t0, NULL);
-    int s = 0;
-    for (int i = -32768; i <= 32767; i++) {
-        s += my_atoi(tests[i+32768]);
+    for (int j = 0; j < 1000; j++) {
+        int s = 0;
+        for (int i = -32768; i <= 32767; i++) {
+            s += my_atoi(tests[i+32768]);
+        }
+        assert(s == -32768);
     }
-    assert(s == -32768);
     struct timeval t1;
     gettimeofday(&t1, NULL);
     double dt = t1.tv_sec - t0.tv_sec + (t1.tv_usec - t0.tv_usec) * 1E-6;
-    printf("%g seconds, %g seconds/op\n", dt, dt/65536);
+    printf("%g seconds, %g seconds/op\n", dt, dt/65536000);
 
 }
 
